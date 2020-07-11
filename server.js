5cae920e244242b66c90dc7783670e625196a870
rudy gauna <rudy.gauna@gmail.com>
2020-07-11 11:07:23 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+require("console.table");
+// const sql = require("./sql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: ""
+});
+
+// connect to the mysql server and sql database
+connection.connect(function (err) {
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  firstPrompt();
+});

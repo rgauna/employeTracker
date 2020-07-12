ba259d374d324049e0f314ef0b199fde88a94d0b
rudy gauna <rudy.gauna@gmail.com>
2020-07-12 18:12:21 -0500

    created schema & seeds sql files

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
-  database: ""
+  password: "GR33n3gg5",
+  database: "employeesDB"
 });
 
 // connect to the mysql server and sql database
